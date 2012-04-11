d2b73baec22483a6437382bfbb8ebcb58446e3db
Eric Jiang <erjiang@indiana.edu>
2012-04-11 12:53:06 -0400

    Added light toggle commands

--- tailremote.py
+++ tailremote.py
@@ -11,16 +11,27 @@ if len(sys.argv) < 3:
 hostname = sys.argv[1]
 filename = sys.argv[2]
 
-#print "Setting up port"
-#port=serial.Serial('/dev/ttyACM0', 9600)
+print "Setting up port"
+port=serial.Serial('/dev/ttyACM0', 9600)
+
+#turn board LEDs off
+port.write("O0")
+port.write("O6")
 
 print "Connecting to remote host"
 sshargs = ["ssh", hostname, "tail", "-f", "-n", "0", filename]
 ssh = subprocess.Popen(sshargs, stdout=subprocess.PIPE)
 
+port.write("I0")
+port.write("I6");
+
 while 1:
     line = ssh.stdout.readline()
     if "GET " in line:
         print "get"
+        port.write("T7")
     elif "POST " in line:
         print "post"
+        port.write("T5")
+    raw=port.read(port.inWaiting())
+    print raw
