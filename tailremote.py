8c46ade403ab9616b69eaa3fe70659fd3fa04f22
Eric Jiang <erjiang@indiana.edu>
2012-04-11 12:57:48 -0400

    Broke commands up to send one char at a time

--- tailremote.py
+++ tailremote.py
@@ -15,23 +15,29 @@ print "Setting up port"
 port=serial.Serial('/dev/ttyACM0', 9600)
 
 #turn board LEDs off
-port.write("O0")
-port.write("O6")
+port.write("O")
+port.write("0")
+port.write("O")
+port.write("6")
 
 print "Connecting to remote host"
 sshargs = ["ssh", hostname, "tail", "-f", "-n", "0", filename]
 ssh = subprocess.Popen(sshargs, stdout=subprocess.PIPE)
 
-port.write("I0")
-port.write("I6");
+port.write("I")
+port.write("0")
+port.write("I")
+port.write("6");
 
 while 1:
     line = ssh.stdout.readline()
     if "GET " in line:
         print "get"
-        port.write("T7")
+        port.write("T")
+        port.write("7")
     elif "POST " in line:
         print "post"
-        port.write("T5")
+        port.write("T")
+        port.write("5")
     raw=port.read(port.inWaiting())
     print raw
